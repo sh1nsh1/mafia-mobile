0427b9678a030170d2b47065f438e02b8e28f7ec
Denis <zak63507@gmail.com>
2026-02-12 17:26:42 +0300

    rename src to app and fix docker compose

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,5 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
-import uvicorn
 
 from api.v1.routers.lobby_router import lobby_router
 from api.v1.routers.room_websocket_router import room_websocket_router
@@ -9,13 +8,7 @@ from infrastructure.websocket.websocket_connection_manager import WebSocketManag
 load_dotenv()
 
 websocket = WebSocketManager()
+
 app = FastAPI()
 app.include_router(lobby_router)
 app.include_router(room_websocket_router)
-
-if __name__ == "__main__":
-    uvicorn.run(
-        app="main:app",
-        host="127.0.0.1",
-        port=8000,
-    )
