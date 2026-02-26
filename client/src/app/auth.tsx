b4fb7bd884e5c3e7fe8d12f901ad1efdba991247
Denis <zak63507@gmail.com>
2026-02-26 12:27:29 +0300

    update expo and change prettier config

--- client/src/app/auth.tsx
+++ client/src/app/auth.tsx
@@ -1,7 +1,7 @@
-import { View, TextInput, Button, StyleSheet } from 'react-native';
-import { useForm, Controller } from 'react-hook-form';
-import * as z from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
+import { View, TextInput, Button, StyleSheet } from "react-native";
+import { useForm, Controller } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const schema = z.object({
   email: z.string(),
@@ -55,7 +55,7 @@ export default function AuthPage() {
         name="password"
       />
 
-      <View style={{ flexDirection: 'row', gap: 8, marginTop: 16 }}>
+      <View style={{ flexDirection: "row", gap: 8, marginTop: 16 }}>
         <Button title="Login" onPress={handleSubmit(onSubmit)} />
         <Button title="Register" onPress={handleSubmit(onSubmit)} />
       </View>
@@ -67,13 +67,13 @@ const styles = StyleSheet.create({
   view: {
     flex: 1,
     gap: 20,
-    backgroundColor: 'whitesmoke',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "whitesmoke",
+    justifyContent: "center",
+    alignItems: "center",
   },
   textInput: {
     fontSize: 14,
     borderBottomWidth: 1,
-    borderColor: 'black',
+    borderColor: "black",
   },
 });
