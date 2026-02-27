be319c12dfeb17b4601a102eb20a9d460473f250
inqlog <not@example.com>
2026-02-27 20:44:32 +0300

    setup new client template with tamagui

--- client/metro.config.js
+++ client/metro.config.js
@@ -1,5 +1,4 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require('expo/metro-config')
 
-/** @type {import('expo/metro-config').MetroConfig} */
-module.exports = getDefaultConfig(__dirname);
+module.exports = getDefaultConfig(__dirname)
