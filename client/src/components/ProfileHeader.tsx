601c085beb2151202c87237fb580c823a5ea29da
inqlog <not@example.com>
2026-03-10 00:51:04 +0300

    minor changes

--- client/src/components/ProfileHeader.tsx
+++ client/src/components/ProfileHeader.tsx
@@ -1,17 +1,15 @@
 import { useAuthStore } from "@stores/auth";
+import { useEffect } from "react";
 import { Avatar, H2, Paragraph, Separator, XStack, YStack } from "tamagui";
 
 export default function ProfileHeader() {
-  const { user } = useAuthStore();
+  const user = useAuthStore(state => state.user);
 
   return (
     <>
       <XStack p="$4" gap="$6" bg="$background">
         <Avatar circular size="$8">
-          <Avatar.Image
-            aria-label="Cam"
-            src="https://images.unsplash.com/photo-1548142813-c348350df52b?&w=150&h=150&dpr=2&q=80"
-          />
+          <Avatar.Image aria-label="Cam" src="" />
           <Avatar.Fallback bg="$red10" />
         </Avatar>
         <YStack>
