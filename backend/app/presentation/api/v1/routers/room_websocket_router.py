8a4a0d80475b5268fc016cb00735b5dd5555d970
sh1nsh1 <maksborisov17@gmail.com>
2026-04-01 21:55:54 +0300

    websocket router & manager fixes

--- backend/app/presentation/api/v1/routers/room_websocket_router.py
+++ backend/app/presentation/api/v1/routers/room_websocket_router.py
@@ -1,11 +1,17 @@
 import logging
+from datetime import datetime, datetime_CAPI
 
 from fastapi import WebSocket, WebSocketDisconnect
 from fastapi.routing import APIRouter
 
+from domain.enums import WebSocketTopicEnum, WebSocketMessageTypeEnum
+from domain.exceptions import DomainException
 from presentation.api.v1.dependencies.alias import CurrentUserWsDep
 from application.services.room_websocket_service import RoomWebSocketServiceDep
 from infrastructure.websocket.dtos.websocket_message import WebSocketMessage
+from infrastructure.websocket.dtos.websocket_game_info_payload import (
+    WebSocketGameInfoPayload,
+)
 
 
 room_websocket_router = APIRouter()
@@ -28,7 +34,21 @@ async def room_websocket(
             raw_message: dict[str, any] = await websocket.receive_json()
             logger.debug(f"{raw_message} {type(raw_message)}")
             ws_message = WebSocketMessage(**raw_message)
-            await room_websocket_service.handle_message(ws_message)
+            try:
+                await room_websocket_service.handle_message(ws_message)
+            except DomainException as e:
+                logger.error(
+                    f"Error on handling message: {ws_message.model_dump()}\nError: {e.args}"
+                )
+                await websocket.send_json(
+                    WebSocketMessage(
+                        message_type=WebSocketMessageTypeEnum.ERROR,
+                        topic=WebSocketTopicEnum(e.topic),
+                        timestamp=datetime.now().isoformat(),
+                        payload=WebSocketGameInfoPayload(text=e.message),
+                    ).model_dump_json()
+                )
+
     except WebSocketDisconnect:
         logger.info(f"{current_user.username} разорвал соединение с комнатой {room_id}")
         await room_websocket_service.unsubscribe_room_webscoket(room_id, current_user)
