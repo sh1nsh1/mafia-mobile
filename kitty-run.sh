fb5ea7a024fd25b5eec654ea7e8196fe1505448d
inqlog <not@example.com>
2026-04-16 11:47:27 +0300

    add --hold to script for kitty

--- kitty-run.sh
+++ kitty-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-kitty @ launch --type=tab --title "Frontend" --cwd="$(pwd)/frontend" --keep-focus sh -c "sleep 2 && npm run dev"
-kitty @ launch --type=tab --title "Backend" --cwd="$(pwd)/backend" --keep-focus sh -c "sleep 1 && uv run fastapi dev"
-kitty @ launch --type=tab --title "Infrastructure" --cwd="$(pwd)" --keep-focus docker compose up postgres redis rustfs
+kitty @ launch --type=tab --title "Frontend" --cwd="$(pwd)/frontend" --keep-focus --hold sh -c "sleep 2 && npm run dev"
+kitty @ launch --type=tab --title "Backend" --cwd="$(pwd)/backend" --keep-focus --hold sh -c "sleep 1 && uv run fastapi dev"
+kitty @ launch --type=tab --title "Infrastructure" --cwd="$(pwd)" --keep-focus --hold docker compose up postgres redis rustfs
